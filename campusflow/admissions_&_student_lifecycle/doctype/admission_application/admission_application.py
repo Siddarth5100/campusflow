57ccd90790aa9d45661ac001d1080ae58a703e00
Siddarth5100 <siddarthsiddhu5100@gmail.com>
2026-04-01 10:35:06 +0530

    feat: add basic validations for admission application doctype

--- campusflow/admissions_&_student_lifecycle/doctype/admission_application/admission_application.py
+++ campusflow/admissions_&_student_lifecycle/doctype/admission_application/admission_application.py
@@ -1,9 +1,45 @@
 # Copyright (c) 2026, JC Siddarth and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class AdmissionApplication(Document):
-	pass
+	def validate(self):
+		# frappe.msgprint("Validate running")
+		# print("____________Test validate")
+
+		# mobile num validation, father field
+		if self.father_number:
+			if len(self.father_number) != 10:
+				frappe.throw("Enter exactly 10 numbers")
+
+		# check is it only number, father field
+		if self.father_number:
+			for num in self.father_number:
+				if not num.isdigit():
+					frappe.throw("Enter only numbers")
+
+		# mobile num validation, mother field
+		if self.mother_number:
+			if len(self.mother_number) != 10:
+				frappe.throw("Enter exactly 10 numbers")
+		
+		# check is it only number, mother field
+		if self.mother_number:
+			for num in self.mother_number:
+				if not num.isdigit():
+					frappe.throw("Enter only numbers")
+
+		# mobile num, validation, guardian
+		if self.guardian_mobile_number:
+			if len(self.guardian_mobile_number) != 10:
+				frappe.throw("Enter exactly 10 numbers")
+
+		# admission type
+		if self.admission_type == "Boarder":
+			if not self.guardian_name:
+				frappe.throw("If Boarder, Guardian details mandatory")
+			
+		
